6af6831d681139a85519af8e3e3aed434a71d60f
ShubhamGupta <92706349+ShubhamGupta2701@users.noreply.github.com>
2023-03-04 18:33:12 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,6 @@
+// let items = ["Shubham","Riya","Tanu","Saurabh",22,[1,2,3,4]];
+let names = ["Shubham","Riya","Tanu","Saurabh"];
+names.push("Manpreet Chutiya");
+console.log(names);
+console.log(names[0]+" & "+names[1]);
+console.log(names.join("-")); 
---
+++ templateLiterals.js
@@ -0,0 +1,6 @@
+let firstName = "Shubham";
+let lastName = "Gupta";
+let course = "MCA";
+let age = 22;
+let template = `Hi my name is ${firstName} ${lastName} , i am ${age} years old and currently i pursue my ${course} form Thapar institute of engineering and technlogy.`;
+console.log(template);
