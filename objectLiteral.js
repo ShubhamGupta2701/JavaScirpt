d5fcf3b09b18c80b975d1ebc49d7df0796fbe3f0
ShubhamGupta <shubham272101@gmail.com>
2024-04-06 05:03:41 +0530

    Update objectLiteral.js

--- objectLiteral.js
+++ objectLiteral.js
@@ -105,18 +105,18 @@ console.log(num1);
 console.log(num2);
 console.log(num1);
 
-let student1 = {name:"Shubham",degree:"MCA",rollno:202101044};
+let student1 = {name:"Shubham",degree:"MCA",rollno:00010};
 // let student2 = student1;  //  this create an exact copy and also update the data inside our new array when we modify our array1.
 let student2 = {...student1};  // --> and thats why we use spread operator .  to overcome this problem. 
-student1.name = "Riya";
-student1.rollno = 202201033;
+student1.name = "chotu";
+student1.rollno = 1111111111;
 console.log(student1);
 console.log(student2);
 
 // similarly we can use this speard operator to object literals.
 /*
 
-let array1 = ["Shubham","Riya"];
+let array1 = ["Shubham","choty"];
 let array2 = array1;
 array1.push("Saurabh");
 //here it change in both the arrays.  and the solution for this problem is to use spread operator to decalare the new array .
